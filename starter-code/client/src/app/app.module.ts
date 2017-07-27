4a035dc5ac89ba1fe9be90f7ece4b04957b5e0cd
salvocostahack <salvatorecostanzopa@gmail.com>
2017-07-27 20:41:10 +0200

    Iteration 1, files node_modules deleted from commit and stage

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {ListdishService} from '../services/listdish.service'
 import { AppComponent } from './app.component';
+import { DisplaydishComponent } from './displaydish/displaydish.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DisplaydishComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ListdishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
