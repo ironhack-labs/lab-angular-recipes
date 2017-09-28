963fd4c093bd079aa6f741fc3060507dcd9b773c
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-28 21:11:32 +0200

    tercera iteraccion

--- starter-code/client/src/app/services/ingredients.service.ts
+++ starter-code/client/src/app/services/ingredients.service.ts
@@ -6,41 +6,16 @@ import 'rxjs/add/operator/map'
 @Injectable()
 export class IngredientsService {
   BASE_URL: string = 'http://localhost:3000';
-  constructor(private http: Http) {}
-//
-//   addIngredient() {
-//     /* CREATE a new Phone. */
-// router.post('/phones', (req, res, next) => {
-//   const thePhone = new Phone({
-//     brand: req.body.brand,
-//     name: req.body.name,
-//     specs: req.body.specs,
-//     image: req.body.image || ''
-//   });
-//
-//   thePhone.save((err) => {
-//     if (err) {
-//       res.json(err);
-//       return;
-//     }
-//
-//     res.json({
-//       message: 'New Phone created!',
-//       id: thePhone._id
-//     });
-//   });
-// });
-//   }
+  constructor(private http: Http) { }
 
   getIngredientsList() {
     return this.http.get(`${this.BASE_URL}/api/ingredients`)
       .map((res: Response) => res.json());
   }
 
-  // get(id) {
-  //   return this.http.get(`${this.BASE_URL}/api/dishes/${id}`)
-  //     .map((res) => res.json());
-  // }
-
+  add(ingredient) {
+    return this.http.post(`${this.BASE_URL}/api/ingredients`, ingredient)
+      .map((res) => res.json());
+  }
 
 }
