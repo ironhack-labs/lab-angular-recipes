6b3b2eb2b6fe769005d0d154d51b44786915553c
trigoporres <santiagotrigoporres@gmail.com>
2017-09-28 20:51:21 +0200

    primera iteracion

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DishesRecipesComponent } from './dishes-recipes/dishes-recipes.component';
+import { DishesService } from './dishes.service'
 
 @NgModule({
   declarations: [
     AppComponent,
+    DishesRecipesComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [DishesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
