3ba9d7d5e8068b5cbd16d080b39e7250d2f0ebed
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-29 08:08:07 +0200

    Iteration 4 | Add Ingredient to Dish done, but save and show the ingredient id instead of the name

--- starter-code/server/config/database.js
+++ starter-code/server/config/database.js
@@ -5,4 +5,3 @@ const dbName = 'recipe-app'
 mongoose.connect(`mongodb://localhost/${dbName}`)
   .then(console.log(`Connected to the ${dbName} database`))
   .catch(console.error.bind(console, 'connection error:'))
-})
