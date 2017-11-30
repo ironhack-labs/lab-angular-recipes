863b38c597f8e505f264a41cfb06a635eed2a1ed
Rocio Garcia <gooblyn@gmail.com>
2017-11-30 21:30:02 +0100

    Adding all files

---
+++ starter-code/client/src/services/dishes.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+
+import {Observable} from 'rxjs';
+import 'rxjs/add/operator/map';
+
+const DOMAIN = "http://localhost:3000";
+const PATH = "/api/dishes";
+const BASEURL = `${DOMAIN}${PATH}`;
+
+@Injectable()
+export class DishesService {
+  constructor(private http: Http) {}
+
+  getDishesList():Observable<any>{
+      return this.http.get(BASEURL)
+                      .map(res => res.json());
+  }
+
+  getDishesById (id):Observable<any>{
+    return this.http.get(`${BASEURL}/${id}`)
+                    .map(res => res.json());
+  }
+}
