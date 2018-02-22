2d3704a8ac6bd2059d9d9833ca70e3d83825d7c0
Ana Recuero <anarecsa@gmail.com>
2018-02-22 19:23:51 +0100

    show all the recipes

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RecipesAllComponent } from './recipes-all/recipes-all.component';
+import { RecipeService } from 'services/recipes.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RecipesAllComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
