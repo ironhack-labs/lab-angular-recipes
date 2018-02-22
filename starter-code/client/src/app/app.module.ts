adf0d478a783c47adf0a70829a35d5a7c838633c
nachobaile <nachobaile@gmail.com>
2018-02-22 19:49:08 +0100

    iteration1 done

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EntryRecipesComponent } from './entry-recipes/entry-recipes.component';
+import { RecipesService } from 'service/recipes.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    EntryRecipesComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [RecipesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
