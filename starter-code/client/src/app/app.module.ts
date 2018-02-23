ed6c240682bdbb65f83805e72ab82d6dfee0c7af
Jose Sanchez <joseansanher@gmail.com>
2018-02-23 10:04:04 +0100

    commit

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Listing } from 'services/listing.service';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [Listing],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
