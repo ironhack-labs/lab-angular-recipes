5501267a1365289f6e8bccf1f2e5849f881e9a72
jesusdelrioc <jesusdelrioc@gmail.com>
2018-02-23 10:00:53 +0100

    iteracion

---
+++ starter-code/client/src/app/recetasola/recetasola.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recetasola',
+  templateUrl: './recetasola.component.html',
+  styleUrls: ['./recetasola.component.css']
+})
+export class RecetasolaComponent implements OnInit {
+
+recipes:any;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
