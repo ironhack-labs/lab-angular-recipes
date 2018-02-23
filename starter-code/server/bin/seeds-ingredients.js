0c2a75ed7f2e9edc6e253d77d96fc7bf8eaf32e2
Santi Ramos <santi92.network@gmail.com>
2018-02-23 11:59:55 +0100

    iteration 3 display ingredients after seeding database

---
+++ starter-code/server/bin/seeds-ingredients.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Ingredient = require('../models/ingredient');
+const Dish = require('../models/dish');
+
+const dbName = 'recipe-app';
+mongoose.connect(`mongodb://localhost/${dbName}`);
+
+const ingredients = [
+    {
+      name: 'salt',
+      description: 'salty'
+    },
+    {
+      name: 'pepper',
+      description: 'spicy'
+    },
+    {
+      name: 'cheese',
+      description: 'cheesy'
+    },
+    {
+      name: 'milk',
+      description: 'dairy'
+    },
+    {
+      name: 'bread',
+      description: 'carbs'
+    },
+    {
+      name: 'water',
+      description: 'h2o'
+    }
+    ]
+
+Ingredient.create(ingredients, (err) => {
+  if (err) { throw(err) }
+  console.log(`Created ${ingredients.length} ingredients`)
+});
