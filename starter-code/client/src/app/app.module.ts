fd76ac7301a525ef02761a17d26528b5a7e316dd
Matt Megarry <mattmegarry@gmail.com>
2018-02-23 16:53:49 +0100

    Half off iteration 2 DONE

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DishesListComponent } from './components/dishes-list/dishes-list.component';
+
+import { RecipesService } from './services/recipes.service';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    DishesListComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [RecipesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
