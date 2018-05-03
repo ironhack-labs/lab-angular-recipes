f6362277d66a58dc380eeaefab3f97147d70e6cd
A-Tr <alvarotrancon@gmail.com>
2018-05-03 23:17:06 +0200

    añadidos seeds chulos

--- starter-code/client/src/app/single-dish/single-dish.component.ts
+++ starter-code/client/src/app/single-dish/single-dish.component.ts
@@ -3,6 +3,7 @@ import { DishesServiceService } from '../dishes-service.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MyDishesComponent } from '../my-dishes/my-dishes.component';
 import { MyIngredientsService } from '../my-ingredients.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-single-dish',
@@ -10,7 +11,7 @@ import { MyIngredientsService } from '../my-ingredients.service';
   styleUrls: ['./single-dish.component.css']
 })
 export class SingleDishComponent implements OnInit {
-  dish: object;
+  dish = {};
   name: string;
 
   ingredients: Array<object>;
@@ -21,7 +22,10 @@ export class SingleDishComponent implements OnInit {
     private ingredientsService: MyIngredientsService
   ) {
     route.params.subscribe(params => {
-      dishesService.getOne(params.id).subscribe(dish => (this.dish = dish));
+      dishesService.getOne(params.id).subscribe(dish => {
+        this.dish = dish;
+        console.log(this.dish);
+      });
     });
     ingredientsService
       .getIngredients()
@@ -30,7 +34,9 @@ export class SingleDishComponent implements OnInit {
 
   addIngredient(dishId, ingId, quantity) {
     console.log(dishId, ingId, quantity);
-    this.dishesService.addIngredient(dishId, ingId, quantity).subscribe();
+    this.dishesService
+      .addIngredient(dishId, ingId, quantity)
+      .subscribe(() => console.log(this.dish));
   }
   ngOnInit() {}
 }
--- starter-code/server/bin/seeds.js
+++ starter-code/server/bin/seeds.js
@@ -106,67 +106,66 @@ const foods = [
 
 const ingredients = [
   {
-    name: 'harina',
-    image: 'https://i.imgur.com/eTmWoAN.png',
+    name: 'Lemon',
+    image: 'http://www.aquilea.com/en/plants/lemon/',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    name: 'patatas',
-    image: 'https://i.imgur.com/DupGBz5.jpg',
+    name: 'Meat',
+    image:
+      'https://cdn.shopify.com/s/files/1/1813/8673/products/Strip_Steak_small_345x345@2x.jpg?v=1500578212',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    name: 'carne',
-    image: 'https://i.imgur.com/hGraGyR.jpg',
+    name: 'Chicken',
+    image:
+      'https://www.freshdirect.com/media/images/product/meat_2/chx_wh_grda_z.jpg?lastModify=2015-01-19',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    name: 'pene',
-    image: 'https://i.imgur.com/',
+    name: 'Pasta',
+    image:
+      'https://s3-eu-west-1.amazonaws.com/stirred/wp-content/uploads/2015/08/16145837/img_1348.jpg',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    name: 'unpocomasdepene',
-    image: 'https://i.imgur.com/WCzJDWz.jpg',
+    name: 'Salt',
+    image: 'https://4.imimg.com/data4/VX/NU/MY-2679528/ankur-salt-500x500.jpg',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    name: 'Chimo Bayo',
-    image: 'https://i.imgur.com/ClxOafl.jpg',
+    name: 'Lettuce',
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/41CGtIyWgML._SX355_.jpg',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    name: 'Hamburger',
-    image: 'https://i.imgur.com/LoG39wK.jpg',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  },
-  {
-    name: 'Gato Crudo',
-    image: 'https://i.imgur.com/5ktcSzF.jpg',
+    name: 'Tomato',
+    image:
+      'https://www.organicfacts.net/wp-content/uploads/2013/05/Organictomato1.jpg',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   }
 ];
+
+Dish.collection.drop();
+Ingredient.collection.drop();
 
 Dish.create(foods, err => {
   if (err) {
     throw err;
   }
   console.log(`Created ${foods.length} dishes`);
-}).then(() => {
-  Ingredient.create(ingredients, err => {
+});
+Ingredient.create(ingredients, err => {
   if (err) {
     throw err;
   }
   console.log(`Created ${ingredients.length} ingredients`);
 });
-})
-
-
