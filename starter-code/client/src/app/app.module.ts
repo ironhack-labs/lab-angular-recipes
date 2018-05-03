0af07bccfe028f2361d90adb277fd6bbeb07dee6
aberouch <a.aberouch@gmail.com>
2018-05-03 21:13:15 +0200

    Iteration 1 and 2 done.

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from "@angular/router";
+import { routes } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { DishListComponent } from './dish-list/dish-list.component';
+import { DishDetailsComponent } from './dish-details/dish-details.component';
+import { DishApiService } from './services/dish-api.service';
+import { IngredientApiService } from './services/ingredient-api.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DishListComponent,
+    DishDetailsComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [DishApiService, IngredientApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ starter-code/client/src/app/dish-details/dish-details.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DishApiService } from '../services/dish-api.service';
+import { IngredientApiService } from '../services/ingredient-api.service';
+
+@Component({
+  selector: 'app-dish-details',
+  templateUrl: './dish-details.component.html',
+  styleUrls: ['./dish-details.component.css'],
+  providers: [ DishApiService, IngredientApiService ]
+})
+export class DishDetailsComponent implements OnInit {
+
+  myDish: any;
+  myIngredients: any;
+
+  constructor(
+    private route : ActivatedRoute,
+    private dishService : DishApiService,
+    private ingredienteService: IngredientApiService,
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params =>{
+      this.getDishDetails(params['id']);
+    });
+
+    this.route
+  }
+
+  getDishDetails(id) {
+    this.ingredienteService.getList()
+      .subscribe((ingredientsResult) => {
+        this.myIngredients = ingredientsResult;
+      });
+    
+    this.dishService.get(id)
+    .subscribe((dishResult) => {
+      console.log(dishResult);
+      this.myDish = dishResult;
+    });
+  }
+
+
+}
