096410eefed5a0bee9b1c3d5205bc4d4632e5c21
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-05-03 20:53:22 +0200

    Hasta iteración 3

---
+++ starter-code/client/src/app/services/dishes.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs";
+import * as _ from "underscore";
+
+import { Dish } from "../list-interface";
+import { Ingredients } from "../ingredients-interface";
+
+@Injectable()
+export class DishesService {
+  BASE_URL: string = "http://localhost:3000";
+  constructor(private http: Http) {}
+  getDishes() {
+    return this.http.get(`${this.BASE_URL}/api/dishes`).map(res => res.json());
+  }
+  getDish(idDish) {
+    return this.http
+      .get(`${this.BASE_URL}/api/dishes/${idDish}`)
+      .map(res => res.json());
+  }
+  getIngredients() {
+    return this.http
+      .get(`${this.BASE_URL}/api/ingredients`)
+      .map(res => res.json());
+  }
+}
