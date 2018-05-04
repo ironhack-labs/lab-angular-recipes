7d6d15c745d36590613b7675dd9563f112535644
Alvaro Reguera <alvaro.reguera@gmail.com>
2018-05-04 09:43:43 +0200

    hasta la 3 incluida

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -2,19 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { ServiceService } from './service.service';
+import { IngredientsService } from './ingredients.service';
+import { RouterModule, Routes } from '@angular/router';
+import { ListDishesComponent } from './list-dishes/list-dishes.component';
+import { SingleComponent } from './single/single.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: ListDishesComponent },
+  { path: 'home/:id', component: SingleComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-  ],
+    ListDishesComponent,
+    SingleComponent
+],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)  //  <!-- "routes" is the array defined above
   ],
-  providers: [],
+  providers: [ServiceService, IngredientsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+
+
+
