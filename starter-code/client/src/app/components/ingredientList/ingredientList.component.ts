8e479753f5f684554ead56f366283ac996c8dd0b
dmarinsa <dmarinsanchez@gmail.com>
2018-07-27 09:48:49 +0200

    delete console.logs

--- starter-code/client/src/app/components/ingredientList/ingredientList.component.ts
+++ starter-code/client/src/app/components/ingredientList/ingredientList.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 export class IngredientListComponent implements OnInit {
   ingredients: Array<Object> = [];
   constructor(private recipeService: RecipesService, private route: ActivatedRoute) { }
-
+  dishId: string = '';
   ngOnInit() {
     this.recipeService.getAllIngredients()
       .subscribe(list => this.ingredients = list)
