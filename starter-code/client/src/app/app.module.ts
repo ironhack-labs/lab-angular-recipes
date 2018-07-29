da57117b348e46df8ec11128c0d0bf40fbfc8e9d
Enrique Pernia <Epernia96@gmail.com>
2018-07-29 21:27:27 +0200

    Iter 2

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -3,18 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { Routes, RouterModule } from '@angular/router';
+import { routes } from './routes';
 import { AppComponent } from './app.component';
+import { ShohDishesComponent } from './shoh-dishes/shoh-dishes.component';
+import { DishesService } from './dishes.service';
+import { SingleDishComponent } from './single-dish/single-dish.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ShohDishesComponent,
+    SingleDishComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [DishesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
